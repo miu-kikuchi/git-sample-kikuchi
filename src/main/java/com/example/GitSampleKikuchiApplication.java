254d53b6af3e5e5d29ac7206ffe22620f060f9c7
kikuchimiu <miu.kikuchi@rakus-partners.co.jp>
2021-11-04 12:29:12 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleKikuchiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKikuchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKikuchiApplication.class, args);
+	}
+
+}
